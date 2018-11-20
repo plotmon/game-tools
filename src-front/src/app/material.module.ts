62edef9bfbb7cf4c9a2bd16984341072c1621410
YK <39464071+plotmon@users.noreply.github.com>
2018-11-21 01:28:41 +0900

    add Angular Material

---
+++ src-front/src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatIconModule
+    , MatButtonModule
+    , MatCheckboxModule
+  ],
+  exports: [
+    MatIconModule
+    , MatButtonModule
+    , MatCheckboxModule
+  ],
+})
+export class MaterialModule { }
